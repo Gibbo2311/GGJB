80ba21d00f7c24ff28a428d6da47a5a5d3ab9d1e
Mark Müller <berlin3d@icloud.com>
2015-01-24 14:10:10 +0100

    added camera script

---
+++ UnityProject/Assets/Scripts/TopDownCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCameraFollow : MonoBehaviour {
+	public Transform _toFollow;
+	public float _height = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = new Vector3 ();
+		v = _toFollow.transform.position;
+		v.y = v.y + _height;	
+		transform.position = v;
+	}
+}
