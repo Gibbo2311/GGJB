8c8ab9660a4ebeb60d7493da27ef721560397899
Scott Gibson <github@scottwgibson.com>
2015-01-24 10:30:08 +0100

    self lighting coolness

---
+++ UnityProject/Assets/Scripts/ControlSelfIllumination.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlSelfIllumination : MonoBehaviour {
+
+	public Color self_color = new Color(0.3f, 1, 0.5f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.renderer.material.color = this.self_color;
+	}
+}
