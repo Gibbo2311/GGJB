22ca0f7e6b0b1a8e466b9f8f2c0653842c4798bc
Mark Müller <berlin3d@icloud.com>
2015-01-24 17:28:09 +0100

    added switch controls twist

--- UnityProject/Assets/Scripts/TwistStatus.cs
+++ UnityProject/Assets/Scripts/TwistStatus.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TwistStatus : MonoBehaviour {
 	public bool _noLightWhileMoventment = false;
 	public bool _reversedControls = false;
+	public bool _switchedControls = false;
 
 	private bool _charMoving = false;
 
@@ -33,6 +34,14 @@ public class TwistStatus : MonoBehaviour {
 		_reversedControls = b;
 	}
 
+	public bool IsSwitchedControls(){
+		return _switchedControls;
+	}
+	
+	public void SetSwitchedControls(bool b){
+		_switchedControls = b;
+	}
+	
 	public void SetCharMoving(bool b) {
 		_charMoving = b;
 	}
